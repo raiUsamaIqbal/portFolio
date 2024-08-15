87c2e59e1c44b575925cb7173f9298d9f72c9cd3
raiUsamaIqbal <154255151+raiUsamaIqbal@users.noreply.github.com>
2024-08-15 10:35:19 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+ let webdes = document.getElementById("web");
+ webdes.style.color = "blue";
+
+ function web(){
+    let button  = document.getElementById("web");
+    button.style.display = "none";
+ }
